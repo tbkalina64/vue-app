de26a3a23ef1e2dc4f0a087e14159bcb6f040582
tbkalina64 <tbkalina64@gmail.com>
2022-01-10 20:01:05 +0200

    added files on project

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>ToDoList</h1>
+    <hr />
+    <TodoList
+    v-bind:todos="todos"
+    />
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+export default {
+  name: 'App',
+  data () {
+    return {
+      todos: [
+        { id: 1, title: 'drink milk', completed: false },
+        { id: 2, title: 'drink tea', completed: false },
+        { id: 3, title: 'drink cola', completed: false }
+      ]
+    }
+  },
+  components: {
+    TodoList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
